a2d57dd4977ddf61487f0ec6ec5ef0cfecd318b6
Daniel Wright <Daniel.Wright@ingka.ikea.com>
2022-01-07 12:23:14 +0100

    cleanup

--- src/Tool.jsx
+++ src/Tool.jsx
@@ -1,67 +1,79 @@
 import React from "react";
 
 export class Tool extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { value: '', result: '' };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = { value: "", result: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    convertToGrid(input) {
-        // https://help.miro.com/hc/en-us/articles/360017572054-Sticky-notes
-        // According to this page, you can only paste up to 50 rows and 100 columns.
+  convertToGrid(input) {
+    // https://help.miro.com/hc/en-us/articles/360017572054-Sticky-notes
+    // According to this page, you can only paste up to 50 rows and 100 columns.
 
-        const inputTripped = input.map((row) => row.trim())
-        const inputFilteredForEmptyRows = inputTripped.filter((row) => row !== "")
-        const inputFilteredForTooLongRows = inputFilteredForEmptyRows.filter((row) => row.length <= 6000)
-        
-        let count = inputFilteredForTooLongRows.length
+    const list = input
+      .map((row) => row.trim())
+      .filter((row) => row !== "")
+      .filter((row) => row.length <= 6000);
 
-        return  {
-            text: inputFilteredForTooLongRows.join("\t"),
-            count
+    return {
+      text: list.join("\t"),
+      count: input.length,
+    };
+  }
+
+  setResult(result) {
+    this.setState({ result });
+    return;
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    const input = this.state.value;
+    const lines = input.split("\n");
+
+    if (lines.length > 5000) {
+      this.setResult(
+        `Too many lines. You entered ${lines.length} but you're only allowed up to 5000.`
+      );
+    } else {
+      const result = this.convertToGrid(lines);
+      navigator.clipboard.writeText(result.text).then(
+        () => {
+          this.setResult(`Copied ${result.count} stickies to clipboard!`);
+        },
+        function (err) {
+          this.setResult("Error copying to clipboard");
         }
+      );
     }
+    event.preventDefault();
+  }
 
-    setResult(result) {
-        this.setState({ result })
-        return
-    }
-
-    handleChange(event) { this.setState({ value: event.target.value }); }
-    handleSubmit(event) {
-        // Handle copying
-        // 5000 Cells
-        // 6000 Characters
-
-        const input = this.state.value
-        const lines = input.split("\n");
-
-        if (lines.length > 5000) {
-            this.setResult(`Too many lines. You entered ${lines.length} but you're only allowed up to 5000.`);
-        } else {
-            const result = this.convertToGrid(lines)
-            navigator.clipboard.writeText(result.text).then(() => {
-                this.setResult(`Copied ${result.count} stickies to clipboard!`)
-            }, function (err) {
-                this.setResult("Error copying to clipboard")
-            });
-        }
-        event.preventDefault();
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Miro sticky-ifier</h1>
-                <p>Convert a list into sticky notes you can paste into Miro.</p>
-                <form onSubmit={this.handleSubmit}>        <label>
-                    <textarea value={this.state.value} cols="50" rows="20" onChange={this.handleChange} />        </label><br />
-                    <input type="submit" value="Copy" />
-                </form>
-                <h2>{this.state.result}</h2>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>Miro sticky-ifier</h1>
+        <p>Convert a list into sticky notes you can paste into Miro.</p>
+        <form onSubmit={this.handleSubmit}>
+          {" "}
+          <label>
+            <textarea
+              value={this.state.value}
+              cols="50"
+              rows="20"
+              onChange={this.handleChange}
+            />{" "}
+          </label>
+          <br />
+          <input type="submit" value="Copy" />
+        </form>
+        <h2>{this.state.result}</h2>
+      </div>
+    );
+  }
 }
